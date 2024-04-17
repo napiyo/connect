fc806b98fc7c9255b6d6a6cd88d29621adc0f2c5
Narendra_Dewasi <radioactivenarendra@gmail.com>
2024-04-18 01:55:35 +0530

    correct npm start command

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ process.on("uncaughtException",(err) =>{
         process.exit(1);
    
 });
-const port = process.env.PORT;
+const port = process.env.PORT | 3000;
 app.listen(port, () => {
     console.log(`App is running... at http://localhost:${port}`);
 });
